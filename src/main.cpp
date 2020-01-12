8c078d5ee92015fd0a994f89f6100299db4779eb
Daniel Meszaros <easimer@blini.easimer.net>
2020-01-12 04:24:14 +0100

    Remove memory test

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,5 @@ extern "C" void kmain(u32 magic, const MB2_Header* mb2) {
         ASSERT(!"Didn't boot from a Multiboot2 bootloader");
     }
 
-    void* test0 = pmalloc(MEM_POOL_KERNEL, 1024);
-    void* test1 = pmalloc(MEM_POOL_KERNEL, 1024);
-    void* test2 = pmalloc(MEM_POOL_USER, 1024);
-    void* test3 = pmalloc(MEM_POOL_USER, 1024);
-
-    logprintf("Kernel test: %x %x User test: %x %x\n", test0, test1, test2, test3);
-
-    pfree(MEM_POOL_USER, test2);
-    pfree(MEM_POOL_USER, test3);
-    pfree(MEM_POOL_KERNEL, test1);
-    pfree(MEM_POOL_KERNEL, test0);
-
     while(1) {}
 }
